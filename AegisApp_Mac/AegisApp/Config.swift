66c7570d39b064062323fcfb02e80014c704eedd
harshitsinghbhandari <24b4506@iitb.ac.in>
2026-03-05 19:35:35 +0530

    chore: native apps shelved - moving to PWA approach

--- AegisApp_Mac/AegisApp/Config.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum Config {
-    static let backendURL = "https://apiaegis.projectalpha.in"
-    static let dashboardURL = "https://aegis.projectalpha.in"
-    static let wsURL = "ws://localhost:8765"
-    static let deviceID = "harshit-macbook"
-}
--- AegisApp_iOS/AegisApp_iOS/YellowConfirmRequest.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct YellowConfirmRequest: Codable, Identifiable {
-    let id: String
-    let action: String
-    let question: String
-    let tool: String
-    let toolkit: String
-}
